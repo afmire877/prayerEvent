dbaea0674c4df28477c6c0077dc21c29e25bb536
afmire877 <33981371+afmire877@users.noreply.github.com>
2019-04-01 23:00:13 +0100

    Added table to app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
-var long, lat;
-const x = document.getElementById("demo");
+let lat, long;
+let constructed = false;
+const d = new Date();
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -9,24 +9,51 @@ function getLocation() {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
-
-const d = new Date();
-
 function showPosition(position) {
   lat = position.coords.latitude;
   long = position.coords.longitude;
 }
-getLocation()
-let url = `http://api.aladhan.com/v1/calendar?latitude=${lat}&longitude=${long}&method=2&month=${d.getMonth}&year=${d.getFullYear}`
-// API fetch 
 
-var XHR = new XMLHttpRequest();
 
-XHR.onstatereadychange = function(){
-	console.log(responseText);
+
+function constructUrl(){
+	getLocation();
+	let url = `http://api.aladhan.com/v1/calendar?latitude=${lat}&longitude=${long}&method=2&month=${d.getMonth()}&year=${d.getFullYear()}`;
+	console.log(url)
+	constructed = true;
+	
 }
-XHR.open('GET', url, true);
-XHR.send();
+
+
+function loadData(){
+	if( constructed == true){
+		var XHR = new XMLHttpRequest();
+
+		XHR.onreadystatechange = function(){
+			if(this.readyState == 4 && this.status == 200){
+
+				 document.getElementById("demo").innerHTML = XHR.responseText;
+			}
+
+		}
+
+		XHR.open('GET', url, true);
+		XHR.send();
+	}
+}
+
+
+
+
+
+
+window.addEventListener("load", constructUrl);
+loadData();
+// window.addEventListener("loadData", loadData);
+
+
+
+
 
 
 
